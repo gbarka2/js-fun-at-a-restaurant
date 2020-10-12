0c9ce1cafedffb73c161bc196b950a20bb841903
Gary Barkauski <gbarka2@gmail.com>
2020-10-11 19:09:04 -0600

    Initial commit

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,16 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant, scott, greetName) {
+    this.name = name;
+    this.restaurant = restaurant;
+
   }
 }
+
+function greetCustomer(greetName) {
+return `Hello ${greetName}!`;
+console.log(`Hello ${greetName}!`);
+}
+
+// how to make a function in a constructor
 
 module.exports = Chef;
